e8e05f0e3b81ff6f7f481e77f8de05e1f89d28ef
Folkert van Heusden <mail@vanheusden.com>
2020-04-02 21:20:00 +0200

    msx 2 vdp work

--- vdp.py
+++ vdp.py
@@ -45,6 +45,11 @@ class vdp(threading.Thread):
 
             self.sc8_rgb_map[i] = [ self.rgb_to_i(r, g, b), r, g, b ]
 
+        self.sourcex = self.sourcey = self.destinationx = self.destinationy = self.numberx = self.numbery = 0
+        self.vdp_cmd = None
+        self.start_destinationx = start_destinationy = 0
+        self.pixelsleft = self.pixeloffset = 0
+
         self.resize_window(320, 192)
         self.resize_trigger: bool = False
 
@@ -78,16 +83,139 @@ class vdp(threading.Thread):
         self.status_register[0] |= 128
 
     def video_mode(self) -> int:
-        m1 = (self.registers[1] >> 4) & 1;
-        m2 = (self.registers[1] >> 3) & 1;
-        m3 = (self.registers[0] >> 1) & 1;
-        m4 = (self.registers[0] >> 2) & 1;
-        m5 = (self.registers[0] >> 3) & 1;
+        m1 = (self.registers[1] >> 4) & 1
+        m2 = (self.registers[1] >> 3) & 1
+        m3 = (self.registers[0] >> 1) & 1
+        m4 = (self.registers[0] >> 2) & 1
+        m5 = (self.registers[0] >> 3) & 1
 
         return (m1 << 4) | (m2 << 3) | (m3 << 2) | (m4 << 1) | m5
 
     def set_register(self, a: int, v: int) -> None:
         self.registers[a] = v
+
+        if a == 0x10:  # palette index
+            self.pal_sel = False
+
+        elif a == 0x20:
+            self.sourcex &= ~255
+            self.sourcex |= v
+
+        elif a == 0x21:
+            self.sourcex &= 255
+            self.sourcex |= (v & 1) << 8
+
+        elif a == 0x22:
+            self.sourcey &= ~255
+            self.sourcey |= v
+
+        elif a == 0x23:
+            self.sourcey &= 255
+            self.sourcey |= (v & 3) << 8
+
+        elif a == 0x24:
+            self.destinationx &= ~255
+            self.destinationx |= v
+
+        elif a == 0x25:
+            self.destinationx &= 255
+            self.destinationx |= (v & 1) << 8
+
+        elif a == 0x26:
+            self.destinationy &= ~255
+            self.destinationy |= v
+
+        elif a == 0x27:
+            self.destinationy &= 255
+            self.destinationy |= (v & 3) << 8
+
+        elif a == 0x28:
+            self.numberx &= ~255
+            self.numberx |= v
+
+        elif a == 0x29:
+            self.numberx &= 255
+            self.numberx |= (v & 3) << 8
+
+        elif a == 0x2a:
+            self.numbery &= ~255
+            self.numbery |= v
+
+        elif a == 0x2b:
+            self.numbery &= 255
+            self.numbery |= (v & 3) << 8
+
+        elif a == 0x2e:  # command register
+            self.vdp_cmd = v >> 4
+            self.pixelsleft = self.numberx * self.numbery
+            self.pixeloffset = 0
+            self.start_destinationx = self.destinationx
+            self.start_destinationy = self.destinationy
+            self.highspeed = False
+
+            print('VDP command: %02x' % self.vdp_cmd)
+
+            if self.vdp_cmd == 0x05:
+                self.put_vdp_2c(self.registers[0x2c])
+
+            elif self.vdp_cmd == 0x07:
+                self.draw_line(self.video_mode(), self.destinationx, self.destinationy, self.numberx, self.numbery, self.registers[0x2d], self.registers[0x2c])
+
+            else:
+                print('(unsupported)')
+            
+            # FIXME
+
+    def put_vdp_2c(self, v):
+        if self.pixelsleft == 0:
+            return
+
+        if self.vdp_cmd == 0x05 or self.vdp_cmd == 0x0b: # logical put pixels, cpu -> vram
+            y = pixeloffset // numberx
+            x = pixeloffset - (y * numberx)
+
+            self.plot(video_mode, destinationx, destinationy, value)
+            pixeloffset += 1
+            pixelsleft -= 1
+
+            destinationx += 1
+            if destinationx == start_destinationx + numberx:
+                destinationx = start_destinationx
+                destinationy += 1
+
+        elif self.vdp_cmd == 0x0f:
+            np = 0
+            dy = pixeloffset // numberx
+            y = destinationy + dy
+            x = destinationx + (pixeloffset - (dy * numberx))
+            offset = 0
+
+            if video_mode == 6:
+                offset = (y * (256 // 2)) + (x // 2)
+            elif video_mode == 1:
+                offset = (y * (512 // 4)) + (x // 4)
+            elif video_mode == 5:
+                offset = (y * (512 // 2)) + (x // 2)
+            elif video_mode == 7:
+                offset = (y * 256) + x
+
+            vdp_mem[offset] = value
+
+            if video_mode == 6:
+                np = 2
+            elif video_mode == 1:
+                np = 4
+            elif video_mode == 5:
+                np = 2
+            elif video_mode == 7:
+                np = 1
+            # FIXME
+
+            pixeloffset += np
+            pixelsleft -= np
+
+        else:
+            print('put_vdp_2c vdp_cmd unsupported %02x' % self.vdp_cmd)
 
     def write_io(self, a: int, v: int) -> None:
         vm = self.video_mode()
@@ -158,6 +286,19 @@ class vdp(threading.Thread):
                 self.rgb[entry] = self.rgb_to_i(r, g, b)
 
             self.pal_sel = not self.pal_sel
+
+        elif a == 0x9b:  # indirect register access port
+            register_index = self.registers[0x11] & 63
+
+            if register_index != 0x11:
+                self.set_register(register_index, v)
+
+            if (self.set_register(0x11) & 128) == 0:
+                self.registers[0x11] += 1
+                self.registers[0x11] &= 63
+
+            if register_index == 0x2c:
+                self.put_vdp_2c(v)
 
         elif a == 0xaa:  # PPI register C
             self.keyboard_row = v & 15
@@ -265,7 +406,7 @@ class vdp(threading.Thread):
             if spx == 0xd0:
                 break
 
-            colori = self.ram[attribute_offset + 3] & 15;
+            colori = self.ram[attribute_offset + 3] & 15
             if colori == 0:
                 continue
 
@@ -273,10 +414,10 @@ class vdp(threading.Thread):
 
             spy = self.ram[attribute_offset + 0]
 
-            pattern_index = self.ram[attribute_offset + 2];
+            pattern_index = self.ram[attribute_offset + 2]
 
             if self.registers[1] & 2:
-                offset = patt + 8 * pattern_index;
+                offset = patt + 8 * pattern_index
 
                 self.draw_sprite_part(spx + 0, spy + 0, offset + 0, rgb, i)
                 self.draw_sprite_part(spx + 0, spy + 8, offset + 8, rgb, i)
@@ -284,7 +425,7 @@ class vdp(threading.Thread):
                 self.draw_sprite_part(spx + 8, spy + 8, offset + 24, rgb, i)
 
             else:
-                self.draw_sprite_part(spx, spy, pattern_index, rgb, i);
+                self.draw_sprite_part(spx, spy, pattern_index, rgb, i)
 
     def poll_kb(self) -> None:
         events = pygame.fastevent.get()
@@ -306,6 +447,151 @@ class vdp(threading.Thread):
             else:
                 print(event)
 
+    def get_pixel(self, vm: int, x: int, y: int, highspeed: bool) -> int:
+        if vm == 6:  # screen 5
+            offset = (y * (256 // 2)) + (x // 2)
+            hnibble = x & 1
+            shift = (1 - hnibble) * 4
+            mask = 15
+
+            if highspeed:
+                return self.ram[offset]
+
+            return (self.ram[offset] >> shift) & mask
+
+        elif vm == 1:  # screen 6
+            offset = (y * (512 // 4)) + (x // 4)
+            hnibble = x & 3
+            shift = (3 - hnibble) * 2
+            mask = 3
+
+            if highspeed:
+                return self.ram[offset]
+
+            return (self.ram[offset] >> shift) & mask
+
+        elif vm == 5:  # screen 7
+            offset = (y * (512 // 2)) + (x // 2)
+            hnibble = x & 1
+            shift = (1 - hnibble) * 4
+            mask = 15
+
+            if highspeed:
+                return self.ram[offset]
+
+            return (self.ram[offset] >> shift) & mask
+
+        elif vm == 7:  # screen 8
+            offset = (y * 256) + x
+
+            if highspeed:
+                return self.ram[offset]
+
+            return self.ram[offset]
+
+        return 123
+
+    def plot(self, vm: int, x: int, y: int, color: int, highspeed: bool) -> None:
+        if x < 0 or y < 0:
+            return
+
+        if vm == 6:  # screen 5
+            if x >= 256 or y >= 212:
+                return
+
+            offset = (y * (256 // 2)) + (x // 2)
+
+            if highspeed:
+                self.ram[offset] = color
+
+            else:
+                bit = x & 1
+                shift = ( 4, 0 )
+                mask = ( 0xf0, 0x0f )
+
+                self.ram[offset] &= mask[bit]
+                self.ram[offset] |= (color & 15) << shift[bit]
+
+        elif vm == 1:  # screen 6
+            if x >= 512 or y >= 212:
+                return
+
+            offset = (y * (512 // 4)) + (x // 4)
+            hnibble = x & 3
+            shift = (3 - hnibble) * 2
+            mask = ~(3 << shift)
+
+            self.ram[offset] &= mask
+            self.ram[offset] |= (color & 3) << shift
+
+        elif vm == 5:  # screen 7
+            if x >= 512 or y >= 212:
+                return
+
+            offset = (y * (512 // 2)) + (x // 2)
+
+            if highspeed:
+                self.ram[offset] = color
+
+            else:
+                hnibble = x & 1
+                shift = (1 - hnibble) * 4
+                mask = ~(15 << shift)
+
+                self.ram[offset] &= mask
+                self.ram[offset] |= (color & 15) << shift
+
+        elif vm == 7:  # screen 8
+            if x >= 256 or y >= 212:
+                return
+
+            offset = (y * 256) + x
+
+            self.ram[offset] = color
+
+    def draw_line(video_mode: int, destinationx: int, destinationy: int, numberx: int, numbery: int, flags: int, color: int):
+        print('draw_line', destinationx, destionationy, numberx, numbery)
+        error = 0.0
+
+        MAJ = flags & 1
+        DIX = flags & 4
+        DIY = flags & 8
+
+        dx = -1 if DIX else 1
+        dy = -1 if DIY else 1
+
+        if MAJ:  # y is longer side
+            deltaerr = numberx / numbery
+            x = destinationx
+            y = destinationy
+
+            while numbery > 0:
+                self.plot(video_mode, x, y, color)
+
+                error += deltaerr
+                if error >= 0.5:
+                    x += dx
+                    error -= 1.0
+
+                y += dy
+                numbery -= 1
+
+        else:
+            deltaerr = numbery / numberx
+            x = destinationx
+            y = destinationy
+
+            while numberx > 0:
+                self.plot(video_mode, x, y, color)
+
+                error += deltaerr
+                if error >= 0.5:
+                    y += dy
+                    error -= 1.0
+
+                x += dx
+                numberx -= 1
+
     def draw_screen_0(self, vm):
         cols = 40 if vm == 16 else 80
 
@@ -325,8 +611,8 @@ class vdp(threading.Thread):
         for map_index in range(0, cols * 24):
             cur_char_nr = self.ram[bg_map + map_index]
 
-            scr_x = (map_index % cols) * 8;
-            scr_y = (map_index // cols) * 8;
+            scr_x = (map_index % cols) * 8
+            scr_y = (map_index // cols) * 8
 
             if cache[cur_char_nr] == None:
                 cache[cur_char_nr] = [ 0 ] * 64
@@ -349,7 +635,7 @@ class vdp(threading.Thread):
         pygame.display.flip()
 
     def draw_screen_1(self):
-        bg_map    = (self.registers[2] &  15) << 10;
+        bg_map    = (self.registers[2] &  15) << 10
         bg_colors = (self.registers[3] & 128) <<  6
         bg_tiles  = (self.registers[4] &   4) << 11
 
@@ -366,11 +652,11 @@ class vdp(threading.Thread):
             cur_char_nr = self.ram[bg_map + map_index]
 
             current_color = self.ram[bg_colors + cur_char_nr // 8]
-            fg = self.rgb[current_color >> 4];
-            bg = self.rgb[current_color & 15];
+            fg = self.rgb[current_color >> 4]
+            bg = self.rgb[current_color & 15]
 
-            scr_x = (map_index % cols) * 8;
-            scr_y = (map_index // cols) * 8;
+            scr_x = (map_index % cols) * 8
+            scr_y = (map_index // cols) * 8
 
             if cache[cur_char_nr] == None:
                 cache[cur_char_nr] = [ 0 ] * 64
@@ -417,8 +703,8 @@ class vdp(threading.Thread):
 
             cur_char_nr = self.ram[bg_map + map_index]
 
-            scr_x = (map_index & 31) * 8;
-            scr_y = ((map_index >> 5) & 31) * 8;
+            scr_x = (map_index & 31) * 8
+            scr_y = ((map_index >> 5) & 31) * 8
 
             if cache[cur_char_nr] == None:
                 cache[cur_char_nr] = [ 0 ] * 64
@@ -455,17 +741,17 @@ class vdp(threading.Thread):
         yo = self.registers[0x17]
 
         for y in range(0, ny):
-                yp = ((y + yo) % 212) * 128
+            yp = ((y + yo) % 212) * 128
 
-                for xp in range(0, 128):
-                        x = xp * 4;
-                        p = name_table + yp + xp
-                        byte = self.ram[p]
+            for xp in range(0, 128):
+                x = xp * 4
+                p = name_table + yp + xp
+                byte = self.ram[p]
 
-                        self.arr[x + 0, y] = self.rgb[byte >> 6]
-                        self.arr[x + 1, y] = self.rgb[(byte >> 4) & 3]
-                        self.arr[x + 2, y] = self.rgb[(byte >> 2) & 3]
-                        self.arr[x + 3, y] = self.rgb[byte & 3]
+                self.arr[x + 0, y] = self.rgb[byte >> 6]
+                self.arr[x + 1, y] = self.rgb[(byte >> 4) & 3]
+                self.arr[x + 2, y] = self.rgb[(byte >> 2) & 3]
+                self.arr[x + 3, y] = self.rgb[byte & 3]
 
         self.draw_sprites()
 
