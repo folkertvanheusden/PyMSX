c07273d0356cad39bab8b4c30c7627df79841477
Folkert van Heusden <mail@vanheusden.com>
2020-04-10 22:58:01 +0200

    real-time clock fixes

--- RP_5C01.py
+++ RP_5C01.py
@@ -7,7 +7,7 @@ from typing import List
 class RP_5C01:
     def __init__(self, debug):
         self.ri: int = 0
-        self.regs: List[int] = [ 0 ] * 16
+        self.blocks = [ [ 0 for k in range(4)] for j in range(16)]
         self.debug = debug
 
     def get_ios(self):
@@ -17,45 +17,60 @@ class RP_5C01:
         return 'RP-5C01 (RTC)'
 
     def read_io(self, a: int) -> int:
-        now = time.localtime()
+        block = self.blocks[0x0d][0] & 3
 
-        if self.ri == 0:
-            return now.tm_sec % 10
-        elif self.ri == 1:
-            return now.tm_sec // 10
-        elif self.ri == 2:
-            return now.tm_min % 10
-        elif self.ri == 3:
-            return now.tm_min // 10
-        elif self.ri == 4:
-            return now.tm_hour % 10
-        elif self.ri == 5:
-            return now.tm_hour // 10
-        elif self.ri == 6:
-            return now.tm_wday
-        elif self.ri == 7:
-            return now.tm_mday % 10
-        elif self.ri == 8:
-            return now.tm_mday // 10
-        elif self.ri == 9:
-            return now.tm_mon % 10
-        elif self.ri == 0x0a:
-            return now.tm_mon // 10
-        elif self.ri == 0x0b:
-            return now.tm_year % 10
-        elif self.ri == 0x0c:
-            return (now.tm_year // 10) % 10
+        rc = 0
 
-        self.debug('RP_5C01: read %02x' % a)
+        if block > 0 or self.ri >= 0x0d:
+            rc = (self.blocks[self.ri][block] & 15)
 
-        return self.regs[self.ri]
+        else:
+            now = time.localtime()
+
+            if self.ri == 0:
+                rc = now.tm_sec % 10
+            elif self.ri == 1:
+                rc = now.tm_sec // 10
+            elif self.ri == 2:
+                rc = now.tm_min % 10
+            elif self.ri == 3:
+                rc = now.tm_min // 10
+            elif self.ri == 4:
+                rc = now.tm_hour % 10
+            elif self.ri == 5:
+                rc = now.tm_hour // 10
+            elif self.ri == 6:
+                rc = now.tm_wday
+            elif self.ri == 7:
+                rc = now.tm_mday % 10
+            elif self.ri == 8:
+                rc = now.tm_mday // 10
+            elif self.ri == 9:
+                rc = now.tm_mon % 10
+            elif self.ri == 0x0a:
+                rc = now.tm_mon // 10
+            elif self.ri == 0x0b:
+                rc = now.tm_year % 10
+            elif self.ri == 0x0c:
+                rc = (now.tm_year // 10) % 10
+
+            self.debug('RP_5C01: read %02x' % a)
+
+        return rc | 0xf0
 
     def write_io(self, a: int, v: int) -> None:
         if a == 0xb4:
             self.ri = v
 
         elif a == 0xb5:
-            self.regs[self.ri] = v
+            block = self.blocks[0x0d][0] & 3
 
-            if self.ri >= 0x0d:
-                self.debug('RP_5C01: write %02x %02x' % (a, v))
+            if block > 0 or self.ri >= 0x0d:
+                if self.ri >= 0x0d:
+                    block = 0
+
+                self.blocks[self.ri][block] = v
+
+                if self.ri == 0x0f:
+                    for i in range(16):
+                        self.blocks[i][1] = 0
